df8c51492f473a1eb2a173e39d3e75b3ed04884e
Logan Devillard <logandvllrd@gmail.com>
2021-12-01 15:15:39 +0100

    Add clock to page

--- Animation/script.js
+++ Animation/script.js
@@ -145,5 +145,31 @@ window.addEventListener('mouseout',
     }
 )
 
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+
+const clock = setInterval(function time()
+{
+    const date = new Date();
+    let hr = date.getHours();
+    let mn = date.getMinutes();
+    let s = date.getSeconds();
+
+    if (hr < 10)
+        hr = '0' + hr;
+    if (mn < 10)
+        mn = '0' + mn;
+    if (s < 10)
+        s = '0' + s;
+
+    hour.textContent = hr;
+    minute.textContent = mn;
+    second.textContent = s;
+
+
+    console.log(hr, mn, s);
+}, 1);
+
 init();
 animate();
