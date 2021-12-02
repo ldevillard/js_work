4088f88f5a2c54e64a44a301b696fc523767636e
Logan Devillard <logandvllrd@gmail.com>
2021-12-02 13:19:58 +0100

    Add button animation

---
+++ Animation/scripts/button.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("button1");
+
+button.addEventListener("click",
+    function()
+    {
+        console.log("button clicked!");
+    }
+)
+
+button.addEventListener("mouseover",
+    function()
+    {
+        button.classList.remove("disactive");
+        button.classList.add("active");
+        console.log("mouse over!");
+    }
+)
+
+button.addEventListener("mouseleave",
+    function()
+    {
+        button.classList.remove("active");
+        button.classList.add("disactive");
+        console.log("mouse leave!");
+    }
+)
